aea46bff93e6f3c30d0db7523896ba3c98f3edbf
PelicanDistress <carteraxwebsolution@gmail.com>
2022-05-07 13:09:26 +0100

    📦 NEW: contract redeployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { Factory as FactoryContract } from '../../generated/Factory/Factory';
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
-export const FACTORY_ADDRESS = '0x0ACB807f76f7efEf7F89488F3297488dCa51a07c';
+export const FACTORY_ADDRESS = '0x2Feb8790000b994a7904667BBa3481D4fa55c7d8';
 
 export const factoryContract = FactoryContract.bind(
   Address.fromString(FACTORY_ADDRESS)
